8acade80713a7a64bb12f0af177ce3830e32024f
Nikhil Chhabra <91406971+procrastinatedbread@users.noreply.github.com>
2023-06-24 03:24:29 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+
+<div>
+<img src=" https://picsum.photos/350/350"/>
+<h1>Raspberry waffle</h1>
+<h3>$12</h3>
+<h4>Raspberries, milk, coconut, flour
+<br/>
+eggs, lemon juice, syrup
+</h4>
+<button>Add to cart</button>
+   
+  
+</div>
+`;
